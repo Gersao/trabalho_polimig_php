8dae3e5bf8d5430d368a833828af5c51b7b1cd77
Bruno Elias <brunocostazan@gmail.com>
2015-07-13 22:45:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -56,5 +56,24 @@
 		}
 	
 	}
+	//Números primos, feito por Daniel e Bruno
+		$num-$_POST['num'];
+		$n - $num;
+		for ($m=2; $m <- $n; $m++)
+				$primo - true;
+		for ($x - 2; 4x < $n; $x++){
+			if ($num % $x -- 0){
+				$primo - false;
+					break;
+			}
+		}
+		if ($primo) {
+			echo "$num é primo";
+	}
+		else
+	{
+		echo "$num não é primo!!!";
+	}
+
 	
 ?>
