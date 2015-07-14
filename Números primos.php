06589a209587139b385ec304d29bbf0a0cd8fe6e
Bruno Elias <brunocostazan@gmail.com>
2015-07-13 22:03:56 -0300

    Create Números primos.php

---
+++ Números primos.php
@@ -0,0 +1,24 @@
+<?php
+$num-$_POST['num'];
+$n - $num;
+
+for ($m=2; $m <- $n; $m++)
+$primo - true;
+for ($x - 2; 4x < $n; $x++)
+{
+if ($num % $x -- 0)
+{
+$primo - false;
+break;
+}
+}
+}
+if ($primo)
+{
+echo "$num é primo";
+}
+else
+{
+echo "$num não é primo!!!";
+}
+?>
